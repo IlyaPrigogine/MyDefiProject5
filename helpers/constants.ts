5cc71dbae5cfbc9ce229065afe18f83ce1428a77
IlyaPrigogine <prigogine@protonmail.com>
2021-12-02 14:22:56 +0800

    Create constants.ts

---
+++ helpers/constants.ts
@@ -0,0 +1,7 @@
+export const dai_ropsten = '0xaD6D458402F60fD3Bd25163575031ACDce07538D';
+export const usdc_ropsten = '0x07865c6E87B9F70255377e024ace6630C1Eaa37F';
+export const comp_ropsten = '';
+export const initialApprove = '10000'
+export const initialSupply = '1000000';
+export const fooAmount = '1.345'
+export const destAddress = '0xE9883A17Ef193241dec09DC213A0D2aaE0462da2';
